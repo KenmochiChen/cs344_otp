a7d2a8b955bdb79108ca976fc6015788e0571556
KenmochiChen <chenchina@outlook.com>
2017-11-28 22:55:08 -0800

    1

--- keygen.c
+++ keygen.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
+#include <ctime>
 
 int main(int argc, char *argv[]){
 
-    
-    printf("%d\n",argc);
-    printf("%s\n",argv[1]);
+    int keygen_len=0;
 
+    if(argc>2){
+        perror("argument wrong\n");
+        exit(1);
+    }
+    keygen_len=atoi(argv[1]);
+    printf("%d\n",keygen_len);
+
+    srand((unsigned)time(0));
+
+    int i;
+    int temp_num;
+    char key[1000]="\0";
+    char temp_key[3]="\0";
+    for(i=0;i<keygen_len;i++){
+        temp_num=rand()%27;
+        if(temp_num==26){
+            strcpy(temp_key," ");
+            strcat(key,temp_key);
+            break;
+        }
+        sprintf(temp_key,"%c",temp_num+65);
+        temp_key[1]='\0';
+        strcat(key,temp_key);
+    }
+    key[keygen_len]='\n';
+    key[keygen_len+1]='\0';
+    printf("%s\n",key);
 }
