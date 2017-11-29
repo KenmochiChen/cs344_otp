5c5cf5ce3ecaff07abf75a57d756481085da87b8
KenmochiChen <chenchina@outlook.com>
2017-11-29 04:34:45 -0800

    1

--- server.c
+++ server.c
@@ -14,8 +14,9 @@ int main(int argc, char *argv[])
 	socklen_t sizeOfClientInfo;
 	char buffer[256];
 	struct sockaddr_in serverAddress, clientAddress;
+	pid_t pid, sid;
 
-	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
+	if (argc =! 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
 
 	// Set up the address struct for this process (the server)
 	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
@@ -34,20 +35,63 @@ int main(int argc, char *argv[])
 	listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
 
 	// Accept a connection, blocking if one is not available until one connects
-	sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
-	establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
-	if (establishedConnectionFD < 0) error("ERROR on accept");
+	
+	while(1){
 
-	// Get the message from the client and display it
-	memset(buffer, '\0', 256);
-	charsRead = recv(establishedConnectionFD, buffer, 255, 0); // Read the client's message from the socket
-	if (charsRead < 0) error("ERROR reading from socket");
-	printf("SERVER: I received this from the client: \"%s\"\n", buffer);
 
-	// Send a Success message back to the client
-	charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
-	if (charsRead < 0) error("ERROR writing to socket");
-	close(establishedConnectionFD); // Close the existing socket which is connected to the client
+		sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+		if (establishedConnectionFD < 0) error("ERROR on accept");
+
+
+		pid = fork();
+		if (pid < 0){
+			error("Server: ERROR forking process");
+			exit(1);
+		}
+
+		// Get the message from the client and display it
+		memset(buffer, '\0', 256);
+		charsRead = recv(establishedConnectionFD, buffer, 255, 0); // Read the client's message from the socket
+		if (charsRead < 0) error("ERROR reading from socket");
+		if (strcmp(buffer, "verification") == 0) {
+				//fprintf(stderr,"invalid client\n");
+				//write error back to client
+
+
+				char response[] = "accept";
+				send(establishedConnectionFD, response, sizeof(accept)-1, 0);
+				
+		} 
+		else {
+				//write confirmation back to client
+				char response[]  = "invalid";
+				send(establishedConnectionFD, response, sizeof(accept)-1, 0);
+				exit(2);
+		}
+		//printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+
+		charsRead = recv(establishedConnectionFD, buffer, 255, 0);
+		printf("-------------\n%s\n",buffer);
+
+		// Send a Success message back to the client
+		charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
+		if (charsRead < 0) error("ERROR writing to socket");
+		close(establishedConnectionFD); // Close the existing socket which is connected to the client
+
+		
+		while (pid > 0){ 	//parent process. wait for children to finish
+			pid = waitpid(-1, &status, WNOHANG);
+		}
+
+
+	}
+	
+
+
+
+
+
 	close(listenSocketFD); // Close the listening socket
 	return 0; 
 }
