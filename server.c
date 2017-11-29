21843ad1fdcece055a4d68b83c08c9deee2c4082
KenmochiChen <chenchina@outlook.com>
2017-11-29 04:36:59 -0800

    1

--- server.c
+++ server.c
@@ -5,12 +5,14 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
 
 void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
 
 int main(int argc, char *argv[])
 {
-	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
+	int listenSocketFD, establishedConnectionFD, portNumber, charsRead, status;
 	socklen_t sizeOfClientInfo;
 	char buffer[256];
 	struct sockaddr_in serverAddress, clientAddress;
@@ -79,7 +81,7 @@ int main(int argc, char *argv[])
 		if (charsRead < 0) error("ERROR writing to socket");
 		close(establishedConnectionFD); // Close the existing socket which is connected to the client
 
-		
+
 		while (pid > 0){ 	//parent process. wait for children to finish
 			pid = waitpid(-1, &status, WNOHANG);
 		}
